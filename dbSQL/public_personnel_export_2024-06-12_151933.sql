bc1ce505c1f37a01c0e5edab0270762069430f14
zonghan <xzonghanx@gmail.com>
2024-06-12 15:21:38 +0800

    dbSQL files

---
+++ dbSQL/public_personnel_export_2024-06-12_151933.sql
@@ -0,0 +1,9 @@
+insert into "personnel" ("name", "nric", "ord", "person_id", "service", "team", "unit", "vocation") values ('xiaohei', '234D', '2024-05-20', 3, 'NSmen', 'R1', '505', 'AFE');
+insert into "personnel" ("name", "nric", "ord", "person_id", "service", "team", "unit", "vocation") values ('xiaohei', '345R', '2024-05-20', 5, 'NSmen', 'R1', '505', 'PNR');
+insert into "personnel" ("name", "nric", "ord", "person_id", "service", "team", "unit", "vocation") values ('xiaoming', '123B', '2024-05-20', 2, 'NSmen', 'R1', '505', 'AFE');
+insert into "personnel" ("name", "nric", "ord", "person_id", "service", "team", "unit", "vocation") values ('xiaoli', '234E', '2024-05-20', 4, 'NSmen', 'A2', '505', 'AFE');
+insert into "personnel" ("name", "nric", "ord", "person_id", "service", "team", "unit", "vocation") values ('xiaole', '123A', '2024-05-21', 1, 'NSmen', 'R1', '505', 'FD ENGR');
+insert into "personnel" ("name", "nric", "ord", "person_id", "service", "team", "unit", "vocation") values ('daxiang', '999Z', '2024-06-09', 28, 'NSmen', 'R2', '508', 'AFE');
+insert into "personnel" ("name", "nric", "ord", "person_id", "service", "team", "unit", "vocation") values ('dongdong', '666F', '2025-04-30', 41, 'NSF', 'R3', '505', 'PNR');
+insert into "personnel" ("name", "nric", "ord", "person_id", "service", "team", "unit", "vocation") values ('bigben', '888H', '2050-04-12', 43, 'REGULAR', 'A2', '507', 'AFE');
+insert into "personnel" ("name", "nric", "ord", "person_id", "service", "team", "unit", "vocation") values ('ting', '555S', '2024-12-31', 47, 'NSF', 'R2', '508', 'FD ENGR');
---
+++ dbSQL/public_qualification_export_2024-06-12_151945.sql
@@ -0,0 +1,6 @@
+insert into "qualification" ("q_code", "q_id", "q_name", "task1", "task2", "task3") values ('RRR01', 2, 'runway repair', 'pothole', 'medium crater', 'large crater');
+insert into "qualification" ("q_code", "q_id", "q_name", "task1", "task2", "task3") values ('AFL01', 3, 'airfield lights', 'replace bulb & fittings', 'replace transformer', 'replace cable');
+insert into "qualification" ("q_code", "q_id", "q_name", "task1", "task2", "task3") values ('RRR02', 4, 'haulage', 'dumptruck', 'excavator', 'compactor');
+insert into "qualification" ("q_code", "q_id", "q_name", "task1", "task2", "task3") values ('AAS01', 1, 'hookwire', 'replace tape', 'replace block', 'disengage wire');
+insert into "qualification" ("q_code", "q_id", "q_name", "task1", "task2", "task3") values ('AAS02', 6, 'barrier', 'replace net', 'replace tape', 'replace stanchion');
+insert into "qualification" ("q_code", "q_id", "q_name", "task1", "task2", "task3") values ('AFL02', 7, 'spal', 'deploy', 'apapi', 'dtgm');
